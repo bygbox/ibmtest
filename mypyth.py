638fa52a97c84ced7805f7f0974fc7fb2396f548
bygbox <131311047+bygbox@users.noreply.github.com>
2024-06-25 15:01:52 +0200

    Add double cycle into pypyth.py
    Another outer for cycle added with extended print() command.

--- mypyth.py
+++ mypyth.py
@@ -1,7 +1,9 @@
 # mypyth.py: does everything magic
 # 240625 13:57
 # 240625 14:06
+# 240625 15:00 double cycle
 print("mypyth.py: start")
-for c in range(10):
-  print(f"c:{c}")
+for k in range(3):
+  for c in range(10):
+    print(f"k:{k} c:{c}")
 print("mypyth.py: finish")
